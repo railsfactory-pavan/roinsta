46a96df0834ba5c8718f6026ed379f9ec53b2fc3
railsfactory-pavan <pavanrf07@gmail.com>
2022-07-18 21:40:47 +0530

    Rails version upgraded 5 to 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users do
     get :avatar, on: :member
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   end
 
   resources :posts
+  resources :photos
   
   resources :photos do
     get :show_images, on: :member
