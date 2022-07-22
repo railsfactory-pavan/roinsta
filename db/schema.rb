745c15baaa5a928bf20536a2f08b70464b6fb1bd
railsfactory-pavan <pavanrf07@gmail.com>
2022-07-22 07:56:31 +0530

    db migrated for locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_20_012342) do
+ActiveRecord::Schema.define(version: 2022_07_22_012520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,19 @@ ActiveRecord::Schema.define(version: 2022_07_20_012342) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.string "company_name"
+    t.string "address"
+    t.string "city"
+    t.string "postcode"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_locations_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.string "body"
     t.bigint "user_id"
@@ -127,6 +140,7 @@ ActiveRecord::Schema.define(version: 2022_07_20_012342) do
   add_foreign_key "followings", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "locations", "users"
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "users"
   add_foreign_key "photos", "posts"
